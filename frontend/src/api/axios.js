f102a6a0489ecbc8ab070b9b7da0202d2ea562fb
Vasanthvasanth29 <vasanthvasanth1945@gmail.com>
2026-03-20 22:39:50 +0530

    fix deploy

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8082',
+    baseURL: "https://halleyx-jyr7.onrender.com",
     headers: {
         'Content-Type': 'application/json',
     },
